e893671e53c1b6db2f759d531df859d1af428bb7
Dmitrii Sikorskii <dmsikorskij@mail.ru>
2019-10-02 18:41:35 +0300

    refractor to async await

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,25 +1,26 @@
 const router = require('express').Router();
 let User = require('../models/user.model');
 
-router.route('/').get((req, res) => {
-  User.find()
-    .then(users => res.json(users))
-    .catch(err => res.status(400).json('Error: ' + err));
+router.route('/').get(async (req, res) => {
+  try {
+    const foundUser = await User.find();
+    res.send(foundUser);
+  } catch (err) {
+    res.status(400).json('Error: ' + err);
+  }
 });
 
-router.post('/add', (req, res) => {
-  console.log(req.body);
+router.post('/add', async (req, res) => {
   const username = req.body.username;
 
-  const newUser = new User({ username });
+  try {
+    const newUser = new User({ username });
 
-  newUser
-    .save()
-    .then(() => res.json(newUser))
-    .catch(err => {
-      console.log(err);
-      res.status(400).json('Error: ' + err);
-    });
+    const savedUser = await newUser.save();
+    res.send(savedUser);
+  } catch (err) {
+    res.status(400).json('Error: ' + err);
+  }
 });
 
 module.exports = router;
